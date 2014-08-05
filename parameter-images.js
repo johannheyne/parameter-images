4be93c982161780160e995512ed120eda898f4fb
johannheyne <mail@johannheyne.de>
2014-08-06 00:28:22 +0200

    js json parsing

---
+++ parameter-images.js
@@ -0,0 +1,8 @@
+jQuery.noConflict();
+jQuery(document).ready(function(){
+
+	//jQuery('.resp').attr( 'data-resp', JSON.stringify(obj).replace(/\"/g, "\'") );
+	
+	console.log( JSON.parse( jQuery('.resp').data('resp').replace(/\'/g, "\"") ) );
+
+});
